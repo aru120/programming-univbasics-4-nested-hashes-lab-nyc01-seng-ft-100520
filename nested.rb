f4334011dcc090f2da7c490e97804188fbbbd026
Kevin Huang <khuang159@gmail.com>
2020-09-14 18:18:46 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -110,6 +110,10 @@ def changing_alan
         :languages => ["C"]
       }
     }
+    
+    programmer_hash[:alan_kay][:known_for] = "GUI"
+    
+    return programmer_hash[:alan_kay][:known_for]
 
 end
 
