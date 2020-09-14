4e429943790db7705ae95f7001d48d676a1cc91f
Kevin Huang <khuang159@gmail.com>
2020-09-14 18:32:03 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -1,4 +1,4 @@
-
+require 'pry'
 def hopper
 	programmer_hash = 
  		{
@@ -15,6 +15,9 @@ def hopper
         :languages => ["C"]
       }
     }
+    
+    
+    return programmer_hash[:grace_hopper]
 
 end
 
@@ -36,7 +39,7 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
-
+return programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,7 +58,7 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
-
+ return programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -67,7 +70,11 @@ def adding_matz
 # return the entire updated hash
 
 	programmer_hash = 
- 		{
+	{
+	  :yukihiro_matsumoto => {
+  :known_for => "Ruby",
+  :languages => ["LISP", "C"]
+ },
       :grace_hopper => {
         :known_for => "COBOL",
         :languages => ["COBOL", "FORTRAN"]
@@ -103,6 +110,12 @@ def changing_alan
         :languages => ["C"]
       }
     }
+    
+    programmer_hash[:alan_kay][:known_for] = "GUI"
+
+return programmer_hash
+    
+    
 
 end
 
@@ -125,5 +138,9 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
+
+programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+
+return programmer_hash
 
 end
