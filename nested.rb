f01785e4e636316659f81de085a8169ec2fb016a
Kevin Huang <khuang159@gmail.com>
2020-09-14 18:16:47 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -16,6 +16,7 @@ def hopper
       }
     }
     
+    
     return programmer_hash[:grace_hopper]
 
 end
@@ -69,7 +70,11 @@ def adding_matz
 # return the entire updated hash
 
 	programmer_hash = 
- 		{
+	:yukihiro_matsumoto => {
+  :known_for => "Ruby",
+   :languages => ["LISP", "C"]
+ },
+	{
       :grace_hopper => {
         :known_for => "COBOL",
         :languages => ["COBOL", "FORTRAN"]
@@ -83,7 +88,7 @@ def adding_matz
         :languages => ["C"]
       }
     }
-
+return programmer_hash
 end
 
 def changing_alan
