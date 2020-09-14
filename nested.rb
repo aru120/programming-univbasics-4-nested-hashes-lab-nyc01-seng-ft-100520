6183b8b28afd006358f2cd2c3d21a349ac519829
Kevin Huang <khuang159@gmail.com>
2020-09-14 18:29:21 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -1,4 +1,4 @@
-
+require 'pry'
 def hopper
 	programmer_hash = 
  		{
@@ -112,6 +112,7 @@ def changing_alan
     }
     
     programmer_hash[:alan_kay][:known_for] = "GUI"
+    binding.pry
     
     
 
